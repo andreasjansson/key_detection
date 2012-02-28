d3a3056a1aba0c873f3dfd4940b1033553ddbe2f
Andreas Jansson <andreas.s.t.jansson@gmail.com>
2012-02-28 01:44:09 +0000

    bit more work on reading mp3s. still nothing works

--- util.py
+++ util.py
@@ -1,4 +1,9 @@
-#import audio_library
+from subprocess import call
+import tempfile
+import scipy.io.wavfile as wavfile
+import scipy.signal as signal
+import os
+from itertools import repeat
 
 class Key:
     def __init__(self, key, time):
@@ -9,13 +14,27 @@ class Key:
 class Algorithm:
 
     def __init__(self, mp3_file):
-        self.audio = self._read_mp3(mp3_file)
+        self.samp_rate, self.audio = Mp3Reader().read_mono(mp3_file)
         self.keys = []
 
     def execute(self):
         raise NotImplementedError()
 
-    def _read_mp3:
-        return [0] * 32768 * 10 # 10 seconds of nothing
 
-
+class Mp3Reader:
+    
+    def read_mono(self, mp3_filename):
+        wav_filename = tempfile.NamedTemporaryFile('w+b', -1, '.wav', 'tmp', None, False)
+        self._mp3_to_wav(mp3_filename, wav_filename)
+        samp_rate, stereo = wavfile.read(wav_filename)
+        os.unlink(wav_filename)
+        mono = stereo[:,0] + stereo[:,1]
+        # pad with zeroes before downsampling
+        mono = mono + repeat(0, 4 - (len(mono) % 4))
+        # downsample
+        mono = signal.resample(mono, len(mono) / 4)
+        return (samp_rate / 4, mono)
+        
+    def _mp3_to_wav(self, mp3_filename, wav_filename):
+        # TODO: UPNEXT
+        pass

1b2b4342539e8120ac0aeb25a37dcd8d59eaf73c
Andreas Jansson <andreas.s.t.jansson@gmail.com>
2012-08-26 12:46:17 +0100

    bugifx

--- keydetection/markov.py
+++ keydetection/markov.py
@@ -255,7 +255,7 @@ def get_test_matrix(mp3, time_limit = None):
     '''
 
     if time_limit:
-        cache = Cache('test_' + time_limit, mp3)
+        cache = Cache('test_%d' % time_limit, mp3)
     else:
         cache = Cache('test', mp3)
     if cache.exists():

d050cdb25460caf67859486d96849e9f0ed2ffe0
Andreas Jansson <andreas.s.t.jansson@gmail.com>
2012-10-06 15:28:15 +0100

    evaluation without time limit

--- workers/evaluate.py
+++ workers/evaluate.py
@@ -49,7 +49,8 @@ def evaluate(filenames_file, models_dir):
             continue
 
         try:
-            test_profile = get_test_profile(mp3_file, time_limit = 30, n = 2)
+            #test_profile = get_test_profile(mp3_file, time_limit = 30, n = 2)
+            test_profile = get_test_profile(mp3_file, n = 2)
 
             if np.sum(test_profile.values) == 0:
                 logging.warning('Silent mp3: %s' % (mp3_file))

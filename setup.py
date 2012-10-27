8574f49e2514a096d4cf99f62943b1c2104c85a4
Andreas Jansson <andreas.s.t.jansson@gmail.com>
2012-10-27 19:21:46 +0100

    basic installer

---
+++ setup.py
@@ -0,0 +1,12 @@
+import os
+from setuptools import setup
+
+setup(
+    name = 'Keydetection',
+    version = '0.2',
+    packages = ['keydetection'],
+    author = 'Andreas Jansson',
+    author_email = 'andreas@jansson.me.uk',
+    description = ('Key detection using Zweiklang profiles'),
+    license = 'GPL v3',
+    )
